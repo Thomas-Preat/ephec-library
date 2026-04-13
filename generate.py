cf8843dd2501a3158ab9816825a2dc25b365bed6
Thomas-Preat <preatthomas@gmail.com>
2026-04-13 11:20:00 +0200

    Updated incorrect file path in generate.py

--- generate.py
+++ generate.py
@@ -1,14 +1,10 @@
 import os
 import json
-import shutil
 
-BASE_DIR = "code"
-DOCS_CODE_DIR = "docs/code"
+BASE_DIR = "docs/code"
 OUTPUT_FILE = "docs/files.json"
 
 data = {}
-
-os.makedirs(DOCS_CODE_DIR, exist_ok=True)
 
 for category in os.listdir(BASE_DIR):
     category_path = os.path.join(BASE_DIR, category)
@@ -18,16 +14,8 @@ for category in os.listdir(BASE_DIR):
 
     data[category] = []
 
-    dest_category = os.path.join(DOCS_CODE_DIR, category)
-    os.makedirs(dest_category, exist_ok=True)
-
     for file in os.listdir(category_path):
         if file.endswith(".py"):
-            src = os.path.join(category_path, file)
-            dst = os.path.join(dest_category, file)
-
-            shutil.copyfile(src, dst)
-
             data[category].append({
                 "name": file,
                 "path": f"code/{category}/{file}"
